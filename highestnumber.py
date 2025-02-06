f90a8e6fd25890c0c363707edaf2b59b6e4a1e98
Fabrizio Méndez <fabrizio.mendez@imprivata.com>
2025-02-06 02:06:12 -0600

    Adding few more exercises

--- highestnumber.py
+++ highestnumber.py
@@ -1,9 +1,7 @@
-
 number_one = int(input("Enter the first number: "))
 number_two = int(input("Enter the second number: "))
 number_three = int(input("Enter the third number: "))
 
-#print the highest number that the user entered, not only the first one
 if number_one > number_two and number_one > number_three:
     print(f"The highest number is: {number_one}")
 elif number_two > number_one and number_two > number_three:
